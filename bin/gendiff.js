ca7e00282a0fe773fabfc39f280dd33cd469938b
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-05-05 12:12:24 +0300

    step 4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1>', 'path to file1')
   .arguments('<filepath2>', 'path to file2')
   .action((filepath1, filepath2) => {
