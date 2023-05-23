9b3139cdad3d07e98b487ea3e39561161f3c1707
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-05-23 16:33:08 +0300

    fix json format case

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,15 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
+import json from './json.js';
 
 const formatStyle = (data, format) => {
   switch (format) {
     case 'stylish':
       return stylish(data);
+    case 'plain':
+      return plain(data);
     case 'json':
-      return JSON.stringify(data);
+      return json(data);
     default:
       throw new Error(`Unknow format ${format}`);
   }
