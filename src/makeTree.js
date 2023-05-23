d393e75881a8754d675e945c4df697727687fd9f
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-05-23 14:49:48 +0300

    fix refactoring

--- src/makeTree.js
+++ src/makeTree.js
@@ -1,9 +1,7 @@
 import _ from 'lodash';
 
 const makeTree = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.sortBy(_.uniq([...keys1, ...keys2]));
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
   const result = keys.map((key) => {
     if (!_.has(data1, key)) {
       return { key, type: 'added', value: data2[key] };
