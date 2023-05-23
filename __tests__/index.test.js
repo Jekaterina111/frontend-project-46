afcf26753d0f5f0d60a64b666f9de81d7547a023
Jekaterina Sutovic <sutovic.jekaterina@gmail.com>
2023-05-23 16:31:05 +0300

    add plain and json format test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,11 +11,19 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 const readFixture = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const stylishResult = readFixture('stylishResult.txt');
+const plainResult = readFixture('plainResult.txt');
+const jsonResult = readFixture('jsonResult.txt');
 
 test.each([
   ['file1.json', 'file2.json', 'stylish', stylishResult],
   ['file1.yaml', 'file2.yaml', 'stylish', stylishResult],
-  ['file1.json', 'file2.json', 'undefined', stylishResult],
+  ['file1.yml', 'file2.yml', 'stylish', stylishResult],
+  ['file1.json', 'file2.json', 'plain', plainResult],
+  ['file1.yml', 'file2.yml', 'plain', plainResult],
+  ['file1.yaml', 'file2.yaml', 'plain', plainResult],
+  ['file1.json', 'file2.json', 'json', jsonResult],
+  ['file1.yaml', 'file2.yaml', 'json', jsonResult],
+  ['file1.yml', 'file2.yml', 'json', jsonResult],
 ])('gendiff', (path1, path2, format, result) => {
   expect(genDiff(getFixturePath(path1), getFixturePath(path2), format)).toEqual(result);
 });
